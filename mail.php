5c1b27e215275a8120fe91485c8c760e5da2561a
zhm <zhm@zhms-MacBook-Pro.local>
2015-02-02 21:49:55 -0500

    My web added

--- mail.php
+++ mail.php
@@ -5,8 +5,12 @@
     </head>
     <body>
     <?php
-        echo "<p>\"data processed\""</>"";
-        echo "hi";
+       $Name = $_POST['name'];
+       $Mail = $_POST['mail'];
+       $Message=$_POST['message'];
+       $to= 'zhmpostbox@live.cn';
+       $subject = 'Comment from myWeb';
+        mail($to.$subject,$Message);
         ?>
         
         
