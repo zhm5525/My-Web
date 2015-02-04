1e04619a11d506c368314334301bcc7becbee469
zhm <zhm@zhms-MacBook-Pro.local>
2015-02-03 21:48:07 -0500

    My web update,interative bar

--- mail.php
+++ mail.php
@@ -5,9 +5,9 @@
     </head>
     <body>
     <?php
-       $Name = $_GET['name'];
-       $Mail = $_GET['mail'];
-       $Message=$_GET['message'];
+       $Name = $_POST['name'];
+       $Mail = $_POST['mail'];
+       $Message=$_POST['message'];
        $to= 'zhmpostbox@live.cn';
        $subject = 'Comment from myWeb';
         mail($to.$subject,$Message);
