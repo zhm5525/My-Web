2fc52117f53bf465b2e030893b765314587f7c99
zhm <zhm@zhms-MacBook-Pro.local>
2015-02-07 17:29:15 -0500

    My web update,new page

--- mail.php
+++ mail.php
@@ -4,38 +4,17 @@
         <titile></titile>
     </head>
     <body>
-         <?php
-        /*
-   
-       $Name = $_REQUEST['name'];
-       $Mail = $_REQUEST['mail'];
-       $Message=$_REQUEST['message'];
+    <?php
+       $Name = $_GET['name'];
+       $Mail = $_GET['mail'];
+       $Message=$_GET['message'];
        $to="zhmpostbox@live.cn";
        $subject = "Comment from myWeb";
-        mail($to,$subject,$Message,"From:".$Mail);
+       $headers = "From: someone@your-website.com"
+        mail($to,$subject,$Message,$headers);
         echo "recived email";
-       
-      */  
-         $from_name ="Mahendra";
-  $from_mail = "xxxx@gmail.com";
-  $to = "zhmpostbox@live.cn";
-  $subject = "Mahendra's Test Mail";
-  $mail_body = "This is email test";
-  $message = $mail_body ;
-  $headers = "MIME-Version: 1.0" . "\r\n";
-  $headers .= "Content-type:text/html;charset=UTF-8\r\n";
-  $headers .= "From: ".$from_name." <".$from_mail.">\r\n";
-  ob_start();
-  $sendmail=mail($to,$subject,$message,$headers);
-
-  if($sendmail)
-  {
-      echo "Send";
-  }
-  else
-  {
-      echo "Not Send";  
-   }
- ?>
+        ?>
+        
+        
     </body>
 </html>
