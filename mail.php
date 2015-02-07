2ff1e1a801952d0838c5221f16b419fe90e501db
zhm <zhm@zhms-MacBook-Pro.local>
2015-02-07 17:21:12 -0500

    My web update,new page

--- mail.php
+++ mail.php
@@ -4,14 +4,16 @@
         <titile></titile>
     </head>
     <body>
-         <?php
-          $Name = $_GET['name'];
+    <?php
+       $Name = $_GET['name'];
        $Mail = $_GET['mail'];
        $Message=$_GET['message'];
        $to="zhmpostbox@live.cn";
        $subject = "Comment from myWeb";
         mail($to,$subject,$Message,"From:".$Mail);
         echo "recived email";
- ?>
+        ?>
+        
+        
     </body>
 </html>
